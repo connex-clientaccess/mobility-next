eec8021f5fb1997c37a62ca48e26984de54bd467
Connex Digital <clientaccess@connex.digital>
2022-10-04 19:34:22 -0400

    added value field

--- pages/index.tsx
+++ pages/index.tsx
@@ -543,7 +543,23 @@ export default function Home() {
                           className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                           id="description"
                           type="text"
-                          placeholder="Enter the Street Address"
+                          placeholder="Lead Description"
+                        ></input>
+                      </div>
+                    </div>
+                    <div className="flex flex-wrap -mx-3 mb-6">
+                      <div className="w-full px-3">
+                        <label
+                          className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                          htmlFor="value"
+                        >
+                          Lead Value
+                        </label>
+                        <input
+                          className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                          id="value"
+                          type="number"
+                          placeholder="20000"
                         ></input>
                       </div>
                     </div>
