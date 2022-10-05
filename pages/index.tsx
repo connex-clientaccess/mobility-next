9701a09adedd0fd7f921d63f7e3a413da9a15bba
Connex Digital <clientaccess@connex.digital>
2022-10-04 20:06:53 -0400

    removed duplicate

--- pages/index.tsx
+++ pages/index.tsx
@@ -416,22 +416,6 @@ export default function Home() {
                         ></input>
                       </div>
                     </div>
-                    <div className="flex flex-wrap -mx-3 mb-6">
-                      <div className="w-full px-3">
-                        <label
-                          className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                          htmlFor="street_address"
-                        >
-                          Street Address
-                        </label>
-                        <input
-                          className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                          id="street_address"
-                          type="text"
-                          placeholder="Enter the Street Address"
-                        ></input>
-                      </div>
-                    </div>
                     <div className="flex flex-wrap -mx-3 mb-2">
                       <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
                         <label
